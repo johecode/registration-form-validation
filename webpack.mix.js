3bb27eb249879b9f48ca59118bc7044e0110e597
Johannes Hentrich <github@johannes-hentrcih.de>
2021-04-05 16:11:05 +0200

    feat: html builder

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,19 @@
 require('laravel-mix-clean')
+require('mix-html-builder')
 const mix = require('laravel-mix')
 
 mix.setPublicPath('dist').clean()
 
-mix.copyDirectory('public', 'dist')
+// mix.copyDirectory('public', 'dist')
+mix.html({
+  htmlRoot: './public/index.html',
+  output: '',
+  partialRoot: './public/partials',
+  minify: {
+    removeComments: true,
+    collapseWhitespace: true
+  }
+})
 
 // set ressource for assets
 mix.setResourceRoot('/')
