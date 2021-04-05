a7ee01198a0b0ae49cb30ff61703395b6974bba0
Johannes Hentrich <github@johannes-hentrcih.de>
2021-04-05 17:34:58 +0200

    fix: submit button enable/disable

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,4 @@
+
 import FormObserver from './class/form-observer.js'
 
 import {
@@ -15,6 +16,7 @@ const form = new FormObserver(formDom)
 // create input for username field
 // const username = form.addInputObserver('username', 'input[name="username"]')
 // username.addValidator('username', ValidatorUsername)
+// const updateUsernameFeedback = state => {}
 
 // create input observer for password field
 const password = form.addInputObserver('password', 'input[name="password"]')
@@ -22,9 +24,28 @@ const password = form.addInputObserver('password', 'input[name="password"]')
 // add validators for password
 password.addValidator('length', value => ValidatorLength(value, 8, 0))
 
+// enable / disable submit button
+const submitDom = formDom.querySelector('input[type="submit"]')
+
+const enableSubmitBtn = () => {
+  if (submitDom) {
+    submitDom.disabled = false
+  }
+}
+
+const disableSubmitBtn = () => {
+  if (submitDom) {
+    submitDom.disabled = true
+  }
+}
+
 // onChange event to update validation feedback
 form.onChange((observerName, state) => {
-  console.log('change', observerName, state)
+  if (form.isValid()) {
+    enableSubmitBtn()
+  } else {
+    disableSubmitBtn()
+  }
 })
 
 form.onSubmit(() => {
